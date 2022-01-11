357f250d45aed09154a10950da9caafc67078b95
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-01-11 17:58:21 +0530

    Remove late load content as not required right away

--- src/browser.ts
+++ src/browser.ts
@@ -1,28 +1,6 @@
 import install from '@layer0/prefetch/window/install'
 import installDevtools from '@layer0/devtools/install'
 
-async function lateLoadContent() {
-  const url = '/content' + window.location.pathname
-  const res = await fetch(url)
-  const html = await res.text()
-  const root = document.createElement('html')
-  root.innerHTML = html
-  const elements = Array.from(document.querySelectorAll('[data-moov-replace]'))
-  const personalizedElements = Array.from(
-    root.querySelectorAll('[data-moov-replace]')
-  )
-
-  for (let i = 0; i < personalizedElements.length; i++) {
-    const el = personalizedElements[i]
-
-    for (let { name, value } of Array.from(el.attributes)) {
-      elements[i].setAttribute(name, value)
-    }
-
-    elements[i].innerHTML = el.innerHTML
-  }
-}
-
 document.addEventListener('DOMContentLoaded', function () {
   // The Layer0 Devtools is a widget that helps developers understand how their site interacts with the Layer0
   // For more details see - https://docs.layer0.co/guides/devtools
@@ -40,17 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
     //       const url = el.getAttribute('href')
     //       if (url) prefetch(url, 'fetch')
     //     },
-    //   },
-    //   {
-    //     selector: '[mw-data-src]',
-    //     callback: el => {
-    //       el.setAttribute('src', el.getAttribute('mw-data-src'))
-    //     }
-    //   },
+    //   }
     // ],
   })
-
-  // if (window.location.pathname.indexOf('/content') !== 0) {
-  //   lateLoadContent()
-  // }
 })
