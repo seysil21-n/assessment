e99de6518ac04c4a6fa1351bce3c191a7b6262dc
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-01-11 18:03:38 +0530

    Remove log content as already being logged

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -38,33 +38,22 @@ new Prefetcher({
 // ENTER REGULAR EXPRESSION SELECTOR FOR IMAGES YOU WANT TO PREFETCH //
 // (usualy as CDN base domain name followed by ".*" as general selecor) //
 
-///////////////////////////////////////////////
-// Callback function for PLP image selector //
+// Callback function for PLP image selector
+
 function deepFetchPLPImages({ $el, el, $ }: DeepFetchCallbackParam) {
   const url = $el.attr('src')
-  console.log(
-    '[][]][][[][]][][][][][[]][[][][]\nPrefetching PLP Hero 1: ' + url + '\n'
-  )
+  console.log(`------------\nPrefetching PLP Hero 1: ${url}------------\n`)
   prefetch(url, 'image')
 }
 
 function deepFetchPLPImagesHero({ $el, el, $ }: DeepFetchCallbackParam) {
   const url = $el.attr('src')
-  console.log(
-    '[][]][][[][]][][][][][[]][[][][]\nPrefetching PLP Hero 2: ' + url + '\n'
-  )
+  console.log(`------------\nPrefetching PLP Hero 2: ${url}------------\n`)
   prefetch(url, 'image')
 }
 
 function deepFetchPLPImagesTwo({ $el, el, $ }: DeepFetchCallbackParam) {
   const url = $el.attr('src')
-  console.log(
-    '[][]][][[][]][][][][][[]][[][][]\nPrefetching PLP: ' + url + '\n'
-  )
+  console.log(`------------\nPrefetching PLP: ${url}------------\n`)
   prefetch(url, 'image')
 }
-
-// function logPrefetchedContent({$el}) { // for testing
-//   // console.log("[][]][][[][]][][][][][[]][[][][]")
-//   console.log("content '"+$el.attr('src')+"' has been prefetched...")
-// }
